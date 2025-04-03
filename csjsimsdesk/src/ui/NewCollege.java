977c50049deaa89cbaf2a6435f641a39b8920f07
Chalew Tesfaye <chalew.t29@gmail.com>
2025-04-03 06:49:21 +0300

    architecture and conceptual class diagram i

--- csjsimsdesk/src/ui/NewCollege.java
+++ csjsimsdesk/src/ui/NewCollege.java
@@ -33,11 +33,11 @@ public class NewCollege extends javax.swing.JDialog {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 534, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 452, Short.MAX_VALUE)
         );
 
         pack();
